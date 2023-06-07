10880880f8f2b2ab56d98b9915ab00a0049695d5
shnguo <shnguo@gmail.com>
2023-06-07 22:27:03 +0800

    oil level

--- send_post.py
+++ send_post.py
@@ -3,7 +3,7 @@ import orjson
 import time
 from log import get_logger
 import redis
-from image_utils import base642image,image2base64
+from image_utils import base642image,image2base64,image2base64_new
 import cv2
 from threading import Thread,local
 import threading
@@ -15,6 +15,7 @@ from datetime import datetime
 import cv2
 import numpy as np
 import requests
+import math
 logger = get_logger(os.path.basename(__file__))
 base_path = os.path.dirname(os.path.abspath(__file__))
 
@@ -66,8 +67,10 @@ class Event_Sender(threading.Thread):
             'silicagel':self.clf_solver,
             'screen_crash':self.clf_solver,
             'damaged':self.clf_solver,
+            'person':self.common_solver,
+            'oil_level':self.oil_level_solver
         }
-        self.url = "http://192.168.0.192:8090/api/v1/result"
+        self.url = "http://localhost:8090/api/v1/result"
         self.kwargs = kwargs
     
     def run(self):
@@ -86,10 +89,10 @@ class Event_Sender(threading.Thread):
              "history_type":self.kwargs["history_type"]
         }
         # print(data)
-        # try:
-        #     requests.post(self.url,json=data,timeout=3)
-        # except Exception as e:
-        #     logger.error(e)
+        try:
+            requests.post(self.url,json=data,timeout=3)
+        except Exception as e:
+            logger.error(e)
 
 
     
@@ -142,12 +145,13 @@ class Event_Sender(threading.Thread):
         post_result = 0
         target_dict = self.result[0]['helmet_suit_smoking']
         if target_dict['img']:
+            # print(target_dict['detection'])
             sign, img = self.helmet_suit_smoking(target_dict['detection'],target='helmet',img = base642image(target_dict['img']))
             if sign:
                 post_result=1
             self.post_report(img,post_result,'helmet') 
         else:
-            self.post_report([],post_result,'')
+            self.post_report(np.array([]),post_result,'')
     
     def suit(self):
         post_result = 0
@@ -158,7 +162,7 @@ class Event_Sender(threading.Thread):
                 post_result=1
             self.post_report(img,post_result,'suit') 
         else:
-            self.post_report([],post_result,'')
+            self.post_report(np.array([]),post_result,'')
 
     def smoking(self):
         post_result=0
@@ -169,7 +173,7 @@ class Event_Sender(threading.Thread):
                 post_result=1
             self.post_report(img,post_result,'suit') 
         else:
-            self.post_report([],post_result,'')
+            self.post_report(np.array([]),post_result,'')
     
     def insulator_broken(self):
         post_result=0
@@ -191,7 +195,7 @@ class Event_Sender(threading.Thread):
                 post_result=1
             self.post_report(img,post_result,'insulator_stain') 
         else:
-            self.post_report([],post_result,'')
+            self.post_report(np.array([]),post_result,'')
 
     def helmet_suit_smoking(self, ls, target, img, confidence_level=0.5):
         ls = np.array([s.split('_') for s in ls]).astype(float)
@@ -199,8 +203,8 @@ class Event_Sender(threading.Thread):
         unique, counts = np.unique(ls[:, 0], return_counts=True)
         
         if target == 'helmet':
-            sign = (1 in unique) and (0 in unique) and (counts[0] < counts[1])
-            for item in ls[np.logical_and(ls[:, 1] > 0.5, ls[:, 0] == 1), :]: # plot rectangle for person.
+            sign = not((1.0 in unique) and (0.0 in unique) and (counts[0] >= counts[1])) 
+            for item in ls[np.logical_and(ls[:, 1] > confidence_level, ls[:, 0] == 1), :]: # plot rectangle for person.
                 img = cv2.rectangle(
                     img, 
                     (int(item[2]), int(item[3])), 
@@ -208,7 +212,7 @@ class Event_Sender(threading.Thread):
                     (255, 0, 0),
                     2
                 )
-            for item in ls[np.logical_and(ls[:, 1] > 0.5, ls[:, 0] == 0), :]: # plot rectangle for helmet.
+            for item in ls[np.logical_and(ls[:, 1] > confidence_level, ls[:, 0] == 0), :]: # plot rectangle for helmet.
                 img = cv2.rectangle(
                     img,
                     (int(item[2]), int(item[3])),
@@ -220,7 +224,7 @@ class Event_Sender(threading.Thread):
         
         if target == 'suit':
             sign = ((1 in unique) and (2 in unique)) or ((1 in unique) and (3 in unique))
-            for item in ls[np.logical_and(ls[:, 1] > 0.5, ls[:, 0] == 1), :]: # plot rectangle for person.
+            for item in ls[np.logical_and(ls[:, 1] > confidence_level, ls[:, 0] == 1), :]: # plot rectangle for person.
                 img = cv2.rectangle(
                     img, 
                     (int(item[2]), int(item[3])), 
@@ -228,7 +232,7 @@ class Event_Sender(threading.Thread):
                     (0, 255, 0),
                     2
                 ) 
-            for item in ls[np.logical_and(ls[:, 1] > 0.5, ls[:, 0] == 2), :]: # plot rectangle for short sleeve.
+            for item in ls[np.logical_and(ls[:, 1] > confidence_level, ls[:, 0] == 2), :]: # plot rectangle for short sleeve.
                 img = cv2.rectangle(
                     img,
                     (int(item[2]), int(item[3])),
@@ -236,7 +240,7 @@ class Event_Sender(threading.Thread):
                     (0, 0, 255),
                     2
                 )
-            for item in ls[np.logical_and(ls[:, 1] > 0.5, ls[:, 0] == 3), :]: # plot rectangle for short pants.
+            for item in ls[np.logical_and(ls[:, 1] > confidence_level, ls[:, 0] == 3), :]: # plot rectangle for short pants.
                 img = cv2.rectangle(
                     img,
                     (int(item[2]), int(item[3])),
@@ -248,7 +252,7 @@ class Event_Sender(threading.Thread):
         
         if target == 'smoking':
             sign = (1 in unique) and (4 in unique)
-            for item in ls[np.logical_and(ls[:, 1] > 0.5, ls[:, 0] == 1), :]: # plot rectangle for person.
+            for item in ls[np.logical_and(ls[:, 1] > confidence_level, ls[:, 0] == 1), :]: # plot rectangle for person.
                 img = cv2.rectangle(
                     img, 
                     (int(item[2]), int(item[3])), 
@@ -256,7 +260,7 @@ class Event_Sender(threading.Thread):
                     (0, 255, 0),
                     2
                 )
-            for item in ls[np.logical_and(ls[:, 1] > 0.5, ls[:, 0] == 4), :]: # plot rectangle for cigarette.
+            for item in ls[np.logical_and(ls[:, 1] > confidence_level, ls[:, 0] == 4), :]: # plot rectangle for cigarette.
                 img = cv2.rectangle(
                     img, 
                     (int(item[2]), int(item[3])), 
@@ -310,12 +314,70 @@ class Event_Sender(threading.Thread):
                     2
                 )
             return sign, img
+    
+    def oil_reading(self,bound1, bound2, center, tail):
+        b1 = np.arcsin((center[1] - bound1[1]) / np.sqrt((center[0] - bound1[0]) ** 2 + (center[1] - bound1[1]) ** 2))
+        if center[0] > bound1[0]:
+            b1 = math.pi - b1
+        if b1 < 0:
+            b1 += 2 * math.pi
+
+        b2 = np.arcsin((center[1] - bound2[1]) / np.sqrt((center[0] - bound2[0]) ** 2 + (center[1] - bound2[1]) ** 2))
+        if center[0] > bound2[0]:
+            b2 = math.pi - b2
+        if b2 < 0:
+            b2 += 2 * math.pi
+
+        r = np.arcsin((center[1] - tail[1]) / np.sqrt((center[0] - tail[0]) ** 2 + (center[1] - tail[1]) ** 2))
+        r = -r
+        if center[0] < tail[0]:
+            r = math.pi - r
+        if r < 0:
+            r += 2 * math.pi
+
+        if b1 > b2:
+            return (b1 - r) / (b1 - b2)
+        return (b2 - r) / (b2 - b1)
+
+    def oil_level(self,ls, img, confidence_level=0.5):
+        """
+        ls: list of string, eg: ['1.0_0.56_968_204_1053_276'] xywh
+        return values: float (percentage), img
+        If no enough points are detected, value will be -1
+        ["bound1", "bound2", "center", "tail"]
+        """
+        ls = np.array([s.split('_') for s in ls]).astype(float)
+        ls = ls[ls[:, 1] > confidence_level, :]
+        unique, counts = np.unique(ls[:, 0], return_counts=True)
+        points = []
+        for item in unique:
+            tmp = ls[ls[:, 0] == item, 2:].mean(axis=0)
+            points.append([(tmp[0]+tmp[2])/2, (tmp[1]+tmp[3])/2])
+            img = cv2.circle(
+                img, 
+                (int(points[int(item)][0]), int(points[int(item)][1])), 
+                radius=5, 
+                color=(0, 255, 255), 
+                thickness=-1
+                            )
+        if len(unique) < 4:
+            return -1, img
+        
+        else:
+            return self.oil_reading(points[0], points[1], points[2], points[3]), img
+
+    def oil_level_solver(self):
+        post_result=0
+        target_dict = self.result[0]['oil_level']
+        if target_dict['img']:
+            result, img = self.insulator(target_dict['detection'],img = base642image(target_dict['img']))
+            self.post_report(img,result,'oil_level') 
 
       
 
 class SendPost(object):
 
-    def __init__(self,source_url,forever,scene,model_list,model_conf,**kwargs):
+    def __init__(self,source_url,forever,scene,model_list,model_conf,pic_area,**kwargs):
         self.detection_list = ['http://0.0.0.0:'+str(model_conf[m]['port'])+'/test' for m in model_list]
         self.source_url = source_url
         self.model_list = model_list
@@ -325,6 +387,7 @@ class SendPost(object):
                         decode_responses=True,socket_timeout=30)
         self.forever = forever
         self.kwargs = kwargs
+        [self.x,self.y,self.w,self.h] = [int(x) for x in pic_area.split(',')]
 
     def read_rtsp_frame(self,key):
         return orjson.loads(self.redis_con.get(key))
@@ -365,7 +428,7 @@ class SendPost(object):
     def run_rtsp_cpu(self):
         cap = cv2.VideoCapture(self.source_url)
         ret,img = cap.read()
-        img  = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
+        # img  = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
         self.detect_and_send(datetime.now().strftime("%Y-%m-%d %H:%M:%S:%f"),img)
 
             
@@ -405,8 +468,12 @@ class SendPost(object):
 
     def detect_and_send(self,pic_time,img_raw_cv):               
         thread_pool = []
+        if self.h>0 and self.w>0:
+            crop = img_raw_cv[self.y:self.y+self.h, self.x:self.x+self.w]
+        else:
+            crop = img_raw_cv
         for url in self.detection_list:
-            thread_pool.append(Detection_Post(img_raw_cv,url))
+            thread_pool.append(Detection_Post(crop,url))
         for t in thread_pool:
             t.start()
             t.join()
@@ -430,6 +497,7 @@ def parse_opt():
     parser.add_argument('--scene',type=str,default='voltage_line_matter',help='scene')
     parser.add_argument('--history_info_id',type=int,default=0,help='history_info_id')
     parser.add_argument('--history_type',type=int,default=0,help='history_type')
+    parser.add_argument('--pic_area',type=str,default='0,0,0,0',help='x,y,w,h')
     opt = parser.parse_args()
     return opt
  
@@ -449,7 +517,7 @@ def main():
         return 0     
     with open(os.path.join(base_path,"model_conf.cfg")) as f:
         model_conf = json.load(f)
-    sp = SendPost(opt['source_url'],opt['forever'],scene,model_list,model_conf,history_info_id=opt['history_info_id'],history_type=opt['history_type'])
+    sp = SendPost(opt['source_url'],opt['forever'],scene,model_list,model_conf,pic_area=opt['pic_area'],history_info_id=opt['history_info_id'],history_type=opt['history_type'])
     sp.run()
     
 
